52afa323adf313711d6fb9196b81339473df19c5
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-05-02 23:29:57 +0530

    Pyth ETH/USD

---
+++ src/pythETHUSD.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import { console2 } from "forge-std/Test.sol";
+import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+
+contract PythETHUSD {
+    IPyth pyth;
+    bytes32 ethUsdPriceId;
+
+    constructor(address _pyth, bytes32 _ethUsdPriceId) {
+        pyth = IPyth(_pyth);
+        ethUsdPriceId = _ethUsdPriceId;
+    }
+
+    function mint() public payable {
+        PythStructs.Price memory price = pyth.getPriceNoOlderThan(
+            ethUsdPriceId,
+            60
+        );
+
+        uint ethPrice18Decimals = (uint(uint64(price.price)) * (10 ** 18))/(10 ** uint8(uint32(-1 * price.expo)));
+        uint oneDollarInWei = ((10 ** 18) * (10 ** 18))/ethPrice18Decimals;
+
+        console2.log("Required payment in wei");
+        console2.log(oneDollarInWei);
+
+        if (msg.value >= oneDollarInWei) {
+
+        } else {
+            revert InsufficientFee();
+        }
+    }
+    error InsufficientFee();
+}
