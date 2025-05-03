82806e8a3c2b94ff3766b06afeda66326ee2f864
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-05-03 23:59:09 +0530

    Pyth Trial

--- script/pythETHUSD.s.sol
+++ script/pythETHUSD.s.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/pythETHUSD.sol";
+import "../src/PythETHUSD.sol";
 
 contract DeployPythETHUSD is Script {
     address constant PYTH_ADDRESS = payable(0xA2aa501b19aff244D90cc15a4Cf739D2725B5729);
 
     function run() public {
-        
+
         vm.startBroadcast();
         PythETHUSD priceFeed = new PythETHUSD(PYTH_ADDRESS);
         console.log("PythETHUSD deployed at:", address(priceFeed));
--- src/pythETHUSD.sol
+++ src/pythETHUSD.sol
@@ -1,20 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
-import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
-import "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
+
+interface IPyth {
+    function getPriceUnsafe(bytes32 id) external view returns (
+        int64 price,
+        uint64 conf,
+        int32 expo,
+        uint256 publishTime
+    );
+}
 
 contract PythETHUSD {
-    IPyth pyth;
+    // Replace with actual Pyth contract address on Base Sepolia
+    address public immutable pythContract;
 
-    constructor(address pythContract) {
-        pyth = IPyth(pythContract);
+    // ETH/USD price feed ID (same on all chains)
+    bytes32 public constant ETH_USD_FEED_ID = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace;
+
+    constructor(address _pythContract) {
+        pythContract = _pythContract;
     }
 
-    function getEthUsdPrice(bytes[] calldata priceUpdateData) public payable returns (PythStructs.Price memory) {
-        uint fee = pyth.getUpdateFee(priceUpdateData);
-        pyth.updatePriceFeeds{value: fee}(priceUpdateData);
-
-        bytes32 priceID = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace;
-        return pyth.getPriceUnsafe(priceID);
+    function getEthUsdPrice() external view returns (
+        int64 price,
+        uint64 conf,
+        int32 expo,
+        uint256 publishTime
+    ) {
+        return IPyth(pythContract).getPriceUnsafe(ETH_USD_FEED_ID);
     }
 }
